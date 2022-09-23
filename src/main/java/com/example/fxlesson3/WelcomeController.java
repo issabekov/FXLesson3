20f5c997f5ffa2239526a28d73b66e57129dab00
Isabekov <zhalgas.sunetullauly@jihc.kz>
2022-09-23 15:31:37 +0600

    First commit Message

---
+++ src/main/java/com/example/fxlesson3/WelcomeController.java
@@ -0,0 +1,34 @@
+package com.example.fxlesson3;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class WelcomeController {
+    @FXML
+    Button logoutbutton;
+
+    public void logoutbuttonClicked(ActionEvent event){
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("login.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 370, 574);
+            Stage stage = new Stage();
+            stage.setTitle("LOGIN PAGE");
+            stage.setScene(scene);
+            stage.show();
+            ((Node)(event.getSource())).getScene().getWindow().hide();
+        }
+        catch (IOException e) {
+            System.out.println(e.toString());
+        }
+    }
+}
